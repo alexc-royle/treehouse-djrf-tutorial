5f304240473e62eb1ff8593422fe293dd2752f3c
Alex <alex@gomo>
2018-09-05 16:24:59 +0100

    Started course, added starting data and updates

--- ed_reviews/urls.py
+++ ed_reviews/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'^api-auth/',
         include('rest_framework.urls', namespace='rest_framework')
     ),
+    url(r'^api/v1/courses/', include('courses.urls', namespace='courses')),
 ]
