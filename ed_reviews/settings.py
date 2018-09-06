544c5993ebf1989ac70585c5ddb4668b5fbe6dbb
Alex <alex@gomo>
2018-09-06 16:10:50 +0100

    token auth

--- ed_reviews/settings.py
+++ ed_reviews/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'courses',
 ]
 
@@ -124,7 +125,7 @@ STATIC_URL = '/static/'
 # REST rest_framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
