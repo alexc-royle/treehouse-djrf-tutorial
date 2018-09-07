090d9ef872f62eb53738c46b4e1c3dfc20aefe52
Alex <alex@gomo>
2018-09-07 15:08:42 +0100

    Single field validation

--- courses/serializers.py
+++ courses/serializers.py
@@ -10,6 +10,10 @@ class ReviewSerializer(serializers.ModelSerializer):
             'id', 'course', 'name', 'email', 'comment', 'rating', 'created_at'
         )
         model = models.Review
+    def validate_rating(self, value):
+        if value in range(1, 6):
+            return value
+        raise serializers.ValidationError('Rating must be an integer between 1 and 5')
 
 class CourseSerializer(serializers.ModelSerializer):
     reviews = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='apiv2:review-detail')
